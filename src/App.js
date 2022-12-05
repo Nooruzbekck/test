c6291a8529ef61f62a2b18cbb9915ba050229d96
Nooruz <rrisovanie638@gmail.com>
2022-12-05 16:57:07 +0600

    test

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "./App.css";
+import Home from "./components/home/Home";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import "react-toastify/dist/ReactToastify.css";
+import TestBuilder from "./components/test/TestBuilder";
+import Tema from "./components/test/Tema";
+import Answer from "./components/answer/Answer";
+import TotalScore from "./components/total/TotalScore";
+
+function App() {
+  return (
+    <div className="App">
+      <HashRouter>
+        {/* <Header /> */}
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/tema" element={<Tema />} />
+          <Route path="/test-build" element={<TestBuilder />} />
+          <Route path="/answer" element={<Answer />} />
+          <Route path="/total_score" element={<TotalScore />} />
+        </Routes>
+        {/* <Footer /> */}
+      </HashRouter>
+    </div>
+  );
+}
+
+export default App;
